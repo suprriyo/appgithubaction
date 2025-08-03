ff6c229bb07a2cda040df023a7e43d5d55b58e01
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2025-08-03 18:02:51 +0530

    fixed

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(-1,1)==0
 
 def test_sub():
-    assert(5,3)==2
-    assert(4,3)==1
-    assert(3,3)==0
-    assert(2,3)==-1
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
